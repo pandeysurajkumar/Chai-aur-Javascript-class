1a9ff64fd0621ac6b5a69c7f7543daee7a59dea5
Suraj Kumar Pandey <pandeysurajkumar265@gmail.com>
2025-03-25 22:29:22 +0530

    learn looping and iife function

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,47 @@
+// if
+
+// const isUserloggedIn = true
+// const temp = 41
+// if(temp < 50){
+//     // execute
+//     console.log("executed");
+    
+// }
+
+// const score = 200
+// if(score >100){
+//     let power ="fly"
+//     console.log(`user power: ${power}`);  //run
+// }
+// // console.log(`user power: ${power}`); //error
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// shorthand
+// const balance = 1000
+// // if(balance > 500 ) console.log("test")  // in same line
+
+// if(balance < 500){
+//     console.log("less than 500");
+// }else if(balance < 750){
+//     console.log("less than 750");
+    
+// }else{
+//     console.log("less than 1200");
+    
+// }
+
+const isUserloggedIn = true
+const debitCard = true
+if(isUserloggedIn && debitCard){
+    console.log("Allowed to buy course");
+    
+}
+
+
+
+
+
+
+
+
---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index+2
+// }
+
+let myarray = ['flash',"batsman","superman"]
+
+// let arr = 0
+// while (arr<myarray.length) {
+//     console.log(myarray[arr]);
+//     // console.log(myarray);
+//     arr++;
+    
+// }
+
+let score = 11
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score <= 10);
